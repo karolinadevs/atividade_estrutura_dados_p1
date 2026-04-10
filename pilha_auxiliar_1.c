e6eebad660d2ac9a86b368745cd4ddcb07770df9
Anna Karolina <156126433+karolinadevs@users.noreply.github.com>
2026-04-10 14:23:46 -0300

    adicionando { } para melhor legibilidade

--- pilha_auxiliar_1.c
+++ pilha_auxiliar_1.c
@@ -1,17 +1,14 @@
-void transfere_pilha(t_pilha *p1, t_pilha *p2)
-{
+void transfere_pilha(t_pilha *p1, t_pilha *p2) {
     t_pilha aux;
     inicializa(&aux);
 
     // Tira de p1 e põe na auxiliar (inverte)
-    while (!pilha_vazia(p1))
-    {
+    while (!pilha_vazia(p1)) {
         empilha(&aux, desempilha(p1));
     }
 
     // Tira da auxiliar e põe em p2 (volta a ordem original)
-    while (!pilha_vazia(&aux))
-    {
+    while (!pilha_vazia(&aux)) {
         empilha(p2, desempilha(&aux));
     }
 }
