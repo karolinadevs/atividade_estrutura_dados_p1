ab2f1f851acb240169d9e399a740075ee94ede7a
Anna Karolina <156126433+karolinadevs@users.noreply.github.com>
2026-04-10 14:16:25 -0300

    adicionando { } para melhor legibilidade

--- vetores_2.c
+++ vetores_2.c
@@ -1,28 +1,27 @@
 #include <stdio.h>
 
-int main()
-{
+int main() {
     int v1[10], v2[10];
     int cont_v2 = 0;
 
     printf("Digite 10 numeros:\n");
-    for (int i = 0; i < 10; i++)
-    {
+    for (int i = 0; i < 10; i++) {
         scanf("%d", &v1[i]);
-        if (v1[i] > 10)
-        {
+        if (v1[i] > 10) {
             v2[cont_v2] = v1[i];
             cont_v2++;
         }
     }
 
     printf("\nVetor Original: ");
-    for (int i = 0; i < 10; i++)
+    for (int i = 0; i < 10; i++) {
         printf("%d ", v1[i]);
-
+    }
+    
     printf("\nVetor Maiores que 10: ");
-    for (int i = 0; i < cont_v2; i++)
+    for (int i = 0; i < cont_v2; i++) {
         printf("%d ", v2[i]);
-
+    }
+    
     return 0;
 }
