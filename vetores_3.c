b6901759b50791d8cd8382be942a04eac0049812
Anna Karolina <156126433+karolinadevs@users.noreply.github.com>
2026-04-10 14:15:38 -0300

    adicionando { } para melhor legibilidade

--- vetores_3.c
+++ vetores_3.c
@@ -1,20 +1,16 @@
 #include <stdio.h>
 
-int main()
-{
+int main() {
     int vetor[10];
 
     printf("Digite 10 numeros:\n");
-    for (int i = 0; i < 10; i++)
-    {
+    for (int i = 0; i < 10; i++){
         scanf("%d", &vetor[i]);
     }
 
     printf("\nMultiplos de 3 encontrados:\n");
-    for (int i = 0; i < 10; i++)
-    {
-        if (vetor[i] % 3 == 0)
-        {
+    for (int i = 0; i < 10; i++) {
+        if (vetor[i] % 3 == 0) {
             printf("Numero: %d | Posicao: %d\n", vetor[i], i);
         }
     }
