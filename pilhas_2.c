ccd3d976b5987b04808ee04301e6f747f410d2e2
Anna Karolina <156126433+karolinadevs@users.noreply.github.com>
2026-04-10 14:22:02 -0300

    adicionando { } para melhor legibilidade

--- pilhas_2.c
+++ pilhas_2.c
@@ -1,27 +1,21 @@
-void separa_pares_impares(t_pilha *p1, t_pilha *pares, t_pilha *impares)
-{
+void separa_pares_impares(t_pilha *p1, t_pilha *pares, t_pilha *impares) {
     t_pilha aux;
     inicializa(&aux);
 
     // Esvazia p1 e distribui
-    while (!pilha_vazia(p1))
-    {
+    while (!pilha_vazia(p1)) {
         int valor = desempilha(p1);
         empilha(&aux, valor); // Guarda na auxiliar para restaurar p1 depois, se necessário
 
-        if (valor % 2 == 0)
-        {
+        if (valor % 2 == 0) {
             empilha(pares, valor);
-        }
-        else
-        {
+        } else {
             empilha(impares, valor);
         }
     }
 
     // Opcional: Restaurar p1
-    while (!pilha_vazia(&aux))
-    {
+    while (!pilha_vazia(&aux)) {
         empilha(p1, desempilha(&aux));
     }
 }
