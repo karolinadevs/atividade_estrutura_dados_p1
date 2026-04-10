e87dbe18a3293333b3a78e66c51710bc94f4e9e2
Anna Karolina <156126433+karolinadevs@users.noreply.github.com>
2026-04-10 14:19:45 -0300

    adicionando { } para melhor legibilidade

--- pilhas_3.c
+++ pilhas_3.c
@@ -1,22 +1,16 @@
 #include <stdio.h>
 #include <string.h>
 
-void inverte_palavras(char texto[])
-{
+void inverte_palavras(char texto[]) {
     t_pilha_char p; // Assuma uma pilha que armazena caracteres
     inicializa_char(&p);
 
-    for (int i = 0; i < strlen(texto); i++)
-    {
-        if (texto[i] != ' ' && texto[i] != '.')
-        {
+    for (int i = 0; i < strlen(texto); i++) {
+        if (texto[i] != ' ' && texto[i] != '.') {
             empilha_char(&p, texto[i]);
-        }
-        else
-        {
+        } else {
             // Quando encontrar espaço ou ponto, desempilha imprimindo
-            while (!pilha_vazia_char(&p))
-            {
+            while (!pilha_vazia_char(&p)) {
                 printf("%c", desempilha_char(&p));
             }
             printf("%c", texto[i]); // Imprime o espaço ou ponto
