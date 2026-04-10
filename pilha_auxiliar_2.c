6b01005e45341da269b649093c046d94779b9f7a
Anna Karolina <156126433+karolinadevs@users.noreply.github.com>
2026-04-10 14:23:22 -0300

    adicionando { } para melhor legibilidade

--- pilha_auxiliar_2.c
+++ pilha_auxiliar_2.c
@@ -1,28 +1,23 @@
-int busca_item(t_pilha *p, int alvo)
-{
+int busca_item(t_pilha *p, int alvo) {
     t_pilha aux;
     inicializa(&aux);
     int posicao = 1;
     int encontrado = -1;
 
     // Procura o item esvaziando a pilha principal
-    while (!pilha_vazia(p))
-    {
+    while (!pilha_vazia(p)) {
         int valor = desempilha(p);
         empilha(&aux, valor);
 
-        if (valor == alvo && encontrado == -1)
-        {
+        if (valor == alvo && encontrado == -1) {
             encontrado = posicao;
         }
         posicao++;
     }
 
     // Restaura a pilha original, desempilhando da auxiliar
-    while (!pilha_vazia(&aux))
-    {
+    while (!pilha_vazia(&aux)) {
         empilha(p, desempilha(&aux));
     }
-
     return encontrado;
 }
