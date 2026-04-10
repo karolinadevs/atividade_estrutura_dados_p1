f9dc87da640af60a4ff0be650eac45c2b1108e5f
anna karolina <jkorumaito@gmail.com>
2026-04-10 10:46:28 -0300

    código corrigido e comentado dizendo o que cada escopo faz

---
+++ pilha_auxiliar_2.c
@@ -0,0 +1,28 @@
+int busca_item(t_pilha *p, int alvo)
+{
+    t_pilha aux;
+    inicializa(&aux);
+    int posicao = 1;
+    int encontrado = -1;
+
+    // Procura o item esvaziando a pilha principal
+    while (!pilha_vazia(p))
+    {
+        int valor = desempilha(p);
+        empilha(&aux, valor);
+
+        if (valor == alvo && encontrado == -1)
+        {
+            encontrado = posicao;
+        }
+        posicao++;
+    }
+
+    // Restaura a pilha original, desempilhando da auxiliar
+    while (!pilha_vazia(&aux))
+    {
+        empilha(p, desempilha(&aux));
+    }
+
+    return encontrado;
+}
